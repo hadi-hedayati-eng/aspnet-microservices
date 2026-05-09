92d7c8592cbfe810ccd5a77c2a97288d576269b9
hadihe2002 <hadihe2002@gmail.com>
2026-05-09 12:57:40 +0330

    Add logging

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -3,6 +3,8 @@ using CommandService.Infrastructure.RabbitMQ;
 using CommandService.Infrastructure.Repositories.Commands;
 using CommandService.Infrastructure.Repositories.Platforms;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +19,25 @@ builder.Services.AddScoped<ICommandRepository, CommandRepository>();
 builder.Services.AddScoped<IPlatformRepository, PlatformRepository>();
 
 builder.Services.AddHostedService<RabbitMQSubscriber>();
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithMachineName()
+    .Enrich.WithEnvironmentName()
+    .WriteTo.Console()
+    .WriteTo.Elasticsearch(
+        new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+        {
+            IndexFormat = "command-service-logs-{0:yyyy-MM}",
+            AutoRegisterTemplate = true,
+            NumberOfReplicas = 0,
+            NumberOfShards = 1
+        }
+    )
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 var app = builder.Build();
 

588ee4cd85c524dc72cf609ae74c605055ed6211
hadihe2002 <hadihe2002@gmail.com>
2026-07-23 23:26:35 +0330

    feat: unit tests

---
+++ PlatformService.UnitTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+using PlatformService.Contracts;
+
+namespace PlatformService.UnitTests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void This_Should_Work()
+    {
+        var myName = "Hadi";
+        myName.Should().BeOfType<string>();
+
+        var platform = new PlatformCreateDto(".NET", "Microsoft", "400$");
+
+        platform.Should().NotBeNull();
+    }
+}
